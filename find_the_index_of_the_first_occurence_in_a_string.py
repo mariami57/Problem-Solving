9ce93ce955d0f12e658838a0abd51e9196062a28
mariami57 <mmitova3@gmail.com>
2025-09-10 21:16:05 +0300

    added more solutions

---
+++ find_the_index_of_the_first_occurence_in_a_string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.find(needle)
+        else:
+            return -1
+
+
+print(Solution().strStr("leetcode", "leeto"))
