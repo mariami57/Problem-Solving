a0c9ac4e341ced5f881efd7caca8452395613cd2
mariami57 <mmitova3@gmail.com>
2025-09-10 21:09:11 +0300

    added more solutions

--- remove_element.py
+++ remove_element.py
@@ -1,11 +1,20 @@
 from typing import List
 
 
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         while val in nums:
+#             nums.remove(val)
+#
+#         return len(nums)
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        while val in nums:
-            nums.remove(val)
-
-        return len(nums)
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
 
 print(Solution().removeElement([0,1,2,2,3,0,4,2], 2))
