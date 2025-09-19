74a4df417da2d40a05cd38ab396864b58f9066dc
mariami57 <mmitova3@gmail.com>
2025-09-19 16:55:43 +0300

    added more solutions

---
+++ Add Binary.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        some_str = []
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -=1
+            if j >= 0:
+                carry += int(b[j])
+                j-=1
+            some_str.append(str(carry % 2))
+            carry //= 2
+        return ''.join(some_str[::-1])
+
+print(Solution().addBinary("100","110010"))
