b164835c46793ecfa3cacc1c282e51543d831035
mariami57 <mmitova3@gmail.com>
2025-09-24 18:05:39 +0300

    added more solutions

--- missing_number.py
+++ missing_number.py
@@ -1,14 +1,22 @@
 from typing import List
 
 
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         unique=[]
+#         for i in range(len(nums)+1):
+#             unique.append(i)
+#
+#         for j in range(len(unique)):
+#             if j not in nums:
+#                 return j
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        unique=[]
-        for i in range(len(nums)+1):
-            unique.append(i)
+        n = len(nums)
+        expected_sum = n * (n+1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
 
-        for j in range(len(unique)):
-            if j not in nums:
-                return j
 
-print(Solution().missingNumber([9,6,4,2,3,5,7,0,1]))
+print(Solution().missingNumber([3,0,1]))
