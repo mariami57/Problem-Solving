657043750f39b48a3c5f3b68574ca2f8d17d6f8c
mariami57 <mmitova3@gmail.com>
2025-09-24 14:23:01 +0300

    updated solution

--- search_insert_position.py
+++ search_insert_position.py
@@ -3,21 +3,17 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-
         left = 0
-        right = len(nums)- 1
-
+        right = len(nums) - 1
+        mid = 0
         while left <= right:
-            middle = (right + left) // 2
-            if nums[middle] == target:
-                return middle
-
-            if nums[middle] < target:
-                left = middle + 1
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
             else:
-                right = middle - 1
+                right = mid - 1
         return left
 
 
 
-print(Solution().searchInsert([1,3,5,6], 7))
+print(Solution().searchInsert([1,3,5,6], 9))
