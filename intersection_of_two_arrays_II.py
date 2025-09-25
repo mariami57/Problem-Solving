239f746df6f4d945914d4362693d3e207fc2099e
mariami57 <mmitova3@gmail.com>
2025-09-25 19:43:53 +0300

    added more solutions

---
+++ intersection_of_two_arrays_II.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        s1 = 0
+        s2 = 0
+        e1 = len(nums1)
+        e2 = len(nums2)
+
+        intersection = []
+
+        while s1 < e1 and s2 < e2:
+            if nums1[s1] == nums2[s2]:
+                intersection.append(nums1[s1])
+                s1 += 1
+                s2 += 1
+            elif nums1[s1] < nums2[s2]:
+                s1 += 1
+            else:
+                s2 += 1
+        return intersection
+
+print(Solution().intersect([4,9,5], [9,4,9,8,4]))
