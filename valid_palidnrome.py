82b4ec92397a6d984d87040e45d96580a0151923
mariami57 <mmitova3@gmail.com>
2025-10-12 19:57:00 +0300

    added more solutions

--- valid_palidnrome.py
+++ valid_palidnrome.py
@@ -1,14 +1,20 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s_list = []
+#
+#         for char in s:
+#             if char.isalnum():
+#                 s_list.append(char.lower())
+#
+#         if s_list == s_list[::-1]:
+#             return True
+#         else:
+#             return False
+
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s_list = []
-
-        for char in s:
-            if char.isalnum():
-                s_list.append(char.lower())
-
-        if s_list == s_list[::-1]:
-            return True
-        else:
-            return False
-
+        s =[c.lower() for c in s if c.isalnum()]
+        return all (s[i] == s[~i] for i in range(len(s)//2))
 print(Solution().isPalindrome("0P"))
