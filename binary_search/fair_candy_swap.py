a78f9866b89617527d9b0a030c9539017cb1a54b
mariami57 <mmitova3@gmail.com>
2025-10-12 17:46:44 +0300

    added more solutions

---
+++ binary_search/fair_candy_swap.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        alice_sum = sum(aliceSizes)
+        bob_sum = sum(bobSizes)
+        diff = (alice_sum - bob_sum) // 2
+        bob_set = set(bobSizes)
+
+        for a in aliceSizes:
+            b = a - diff
+            if b in bob_set:
+                return [a, b]
+        return []
+
+print(Solution().fairCandySwap(aliceSizes = [2], bobSizes = [1,3]))
+
+
+
