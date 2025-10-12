934f7968cff32f923c574a2e0ed61343c0e35eff
mariami57 <mmitova3@gmail.com>
2025-10-12 19:20:48 +0300

    added more solutions

---
+++ merge_similar_items.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        items1.extend(items2)
+        items1.sort()
+
+        ret = []
+
+        for value, weight in items1:
+            if ret and ret[-1][0] == value:
+                ret[-1][1] += weight
+            else:
+                ret.append([value, weight])
+
+        return ret
+
+print(Solution().mergeSimilarItems(items1 = [[1,3],[2,2]], items2 = [[7,1],[2,2],[1,4]]))
