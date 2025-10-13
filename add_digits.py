735c698537dcd7d727e52832cd7a657f83c4fa2f
mariami57 <mmitova3@gmail.com>
2025-10-13 19:05:19 +0300

    added more solutions

--- add_digits.py
+++ add_digits.py
@@ -1,9 +1,22 @@
+# class Solution:
+#     def addDigits(self, num: int) -> int:
+#         if num == 0:
+#             return 0
+#         if num % 9 == 0:
+#             return 9
+#         else:
+#             return (num % 9)
+
+
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
-        if num % 9 == 0:
-            return 9
-        else:
-            return (num % 9)
+        if num < 10:
+            return num
 
+        sum = 0
+        while num > 0:
+            sum += num % 10
+            num = num // 10
+        return self.addDigits(sum)
+
+print(Solution().addDigits(38))
