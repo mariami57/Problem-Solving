2bca0dbb7093ee03f97d258b7226673d7b67c76e
mariami57 <mmitova3@gmail.com>
2025-10-13 21:36:22 +0300

    added more solutions

--- intersection_of_two_linked_lists.py
+++ intersection_of_two_linked_lists.py
@@ -7,25 +7,52 @@ class ListNode:
         self.val = x
         self.next = None
 
+# class Solution:
+#     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+#         stack1, stack2 = [], []
+#
+#         def fill_stack(stack, cur):
+#             while cur:
+#                 stack.append(cur)
+#                 cur = cur. next
+#
+#         fill_stack(stack1, headA)
+#         fill_stack(stack2, headB)
+#
+#         prev = None
+#         while stack1 and stack2:
+#             node1, node2 = stack1.pop(), stack2.pop()
+#             if node1 == node2:
+#                 prev=node1
+#
+#         return prev
+
+
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        stack1, stack2 = [], []
 
-        def fill_stack(stack, cur):
+        def find_length(cur):
+            length = 0
             while cur:
-                stack.append(cur)
-                cur = cur. next
+                length += 1
+                cur = cur.next
+            return length
 
-        fill_stack(stack1, headA)
-        fill_stack(stack2, headB)
+        length1 = find_length(headA)
+        length2 = find_length(headB)
 
-        prev = None
-        while stack1 and stack2:
-            node1, node2 = stack1.pop(), stack2.pop()
-            if node1 == node2:
-                prev=node1
+        while length1 > length2:
+            headA = headA.next
+            length1 -=1
+        while length1 < length2:
+            headB = headB.next
+            length2 -=1
 
-        return prev
+        while headA != headB:
+            headA = headA.next
+            headB = headB.next
+        return headA
+
 
 
 
