76d95b31a50efa39fe504e7961b694d061a2597a
mariami57 <mmitova3@gmail.com>
2025-10-14 10:41:12 +0300

    added more solutions

--- happy_number.py
+++ happy_number.py
@@ -2,18 +2,19 @@
 
 class Solution:
     def isHappy(self, n: int) -> bool:
-        if n==1 or n==7:
+        if n == 1 or n == 7:
             return True
-        elif (n < 10):
+        elif n < 10:
             return False
         else:
             sum = 0
             while n > 0:
-                temp = n % 10
-                sum += temp * temp
+                dig1 = n % 10
+                sum += dig1 * dig1
                 n = n // 10
             return self.isHappy(sum)
 
-print(Solution().isHappy(19))
+
+print(Solution().isHappy(10))
 
 
