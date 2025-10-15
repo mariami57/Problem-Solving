e071ca4c14c9c6662ddab2522b6e0457eb6342ae
mariami57 <mmitova3@gmail.com>
2025-10-15 16:10:59 +0300

    added more solutions

--- palindrome_linked_list.py
+++ palindrome_linked_list.py
@@ -9,22 +9,40 @@ class ListNode:
 from typing import Optional
 
 
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         list_vals = []
+#
+#         while head:
+#             list_vals.append(head.val)
+#             head = head.next
+#
+#         return all(list_vals[i] == list_vals[~i] for i in range(len(list_vals) // 2))
+
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        list_vals = []
+        slow = head
+        fast = head
 
-        while head:
-            list_vals.append(head.val)
-            head = head.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-        return all(list_vals[i] == list_vals[~i] for i in range(len(list_vals) // 2))
+        node = None
 
+        while slow:
+            temp = slow.next
+            slow.next = node
+            node = slow
+            slow = temp
 
-node = None
-while head:
-    temp = head.next
-    head.next = node
-    node = head
-    head = temp
+        first, second = head, node
+        while second:
+            if first.val != second.val:
+                return False
 
-return node
+            first = first.next
+            second = second.next
+
+        return True
