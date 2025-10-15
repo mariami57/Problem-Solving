ca4e3723092907ae7232e0e76a268c5f7586f218
mariami57 <mmitova3@gmail.com>
2025-10-15 19:28:33 +0300

    added more solutions

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,18 +1,27 @@
 from typing import List
 
 
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#
+#         for i in range(len(nums)):
+#             if nums[i] == 0:
+#                 nums.remove(nums[i])
+#                 nums.append(0)
+#
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        next_idx = 0
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums.remove(nums[i])
-                nums.append(0)
-
-
+        for idx in range(len(nums)):
+            if nums[idx] != 0:
+                nums[idx], nums[next_idx] = nums[next_idx], nums[idx]
+                next_idx += 1
         return nums
 
-print(Solution().moveZeroes([0,1,0,3,12]))
+
+print(Solution().moveZeroes([1,0,1]))
