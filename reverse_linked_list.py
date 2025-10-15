3e004979f6a249ec9cf955fb534829145d03c8f8
mariami57 <mmitova3@gmail.com>
2025-10-15 16:16:51 +0300

    updated solution

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -17,3 +17,29 @@ class Solution:
             head = temp
 
         return node
+
+def build_linked_list(values):
+    if not values:
+        return None
+    head = ListNode(values[0])
+    current = head
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
+
+
+def linked_list_to_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+
+
+head = build_linked_list([1, 2, 3, 4, 5])
+reversed_head = Solution().reverseList(head)
+
+
+print(linked_list_to_list(reversed_head))
