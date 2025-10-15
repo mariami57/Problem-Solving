49ba1d088fb77b9ccf7adac42aa847aa9e89c97f
mariami57 <mmitova3@gmail.com>
2025-10-15 13:50:30 +0300

    added more solutions

---
+++ palindrome_linked_list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        list_vals = []
+
+        while head:
+            list_vals.append(head.val)
+            head = head.next
+
+        return all(list_vals[i] == list_vals[~i] for i in range(len(list_vals) // 2))
+
+
+
