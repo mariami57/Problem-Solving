3344dac03b30266d21b2dcc54555742d6c14447e
mariami57 <mmitova3@gmail.com>
2025-10-17 13:24:03 +0300

    added more solutions

--- faulty_keyboard.py
+++ faulty_keyboard.py
@@ -1,11 +1,9 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        l = list(s)
-        for index in range(len(l)):
-            if l[index] == 'i':
-                l[:index] = l[:index][::-1]
-        while 'i' in l:
-            l.remove('i')
-        return "".join(l)
-
+        while "i" in s:
+            index = s.index("i")
+            first = s[:index][::-1]
+            last = s[index+1:]
+            s = "".join([first,last])
+        return s
 print(Solution().finalString(s = "poiinter"))
