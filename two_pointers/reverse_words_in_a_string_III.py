3686248c40afdaa1d0a78c186d769069f7298326
mariami57 <mmitova3@gmail.com>
2025-10-22 21:15:50 +0300

    added updated solution

--- two_pointers/reverse_words_in_a_string_III.py
+++ two_pointers/reverse_words_in_a_string_III.py
@@ -6,6 +6,24 @@ class Solution:
         for word in s:
            new_s.append(word[::-1])
 
-        return ' '.join(new_s)
+# class Solution:
+#     def reverseWords(self, s: str) -> str:
+#         word = []
+#
+#         start = 0
+#         end = 0
+#
+#         while end < len(s):
+#             if s[end] == ' ':
+#                 word.append(s[start:end][::-1])
+#                 start = end + 1
+#             if end == len(s)-1:
+#                 word.append(s[start:end+1][::-1])
+#                 start = end + 1
+#             end+=1
+#         return ' '.join(word)
+
+
+
 
 print(Solution().reverseWords(s = "Let's take LeetCode contest"))
