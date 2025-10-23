41b0136a18d57df89f6f1c118bd6ca04adbe68b8
mariami57 <mmitova3@gmail.com>
2025-10-23 09:21:40 +0300

    added more solutions

---
+++ two_pointers/reverse_vowels_of_a_string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = ['a', 'e', 'i', 'o',  'u']
+
+        s = list(s)
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left].lower() in vowels and s[right].lower() in vowels:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+            if s[left].lower() not in vowels:
+                left += 1
+            if  s[right].lower() not in vowels:
+                right -= 1
+
+
+        return ''.join(s)
+
+
+print(Solution().reverseVowels(s = "IceCreAm"))
+
