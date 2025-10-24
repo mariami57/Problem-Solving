484d5c760f6d767ce6cdc9326c85c910b5345425
mariami57 <mmitova3@gmail.com>
2025-10-24 19:21:56 +0300

    added additional solutions

--- two_pointers/crawler_log_folder.py
+++ two_pointers/crawler_log_folder.py
@@ -2,17 +2,30 @@ import re
 from typing import List
 
 
+# class Solution:
+#     def minOperations(self, logs: List[str]) -> int:
+#         folder_depth = 0
+#         pattern = r'[a-z0-9\/]'
+#         for i in range(len(logs)):
+#             if logs[i] == '../' and folder_depth > 0:
+#                 folder_depth -= 1
+#             elif logs[i] == './':
+#                 continue
+#
+#             if re.match(pattern, logs[i]):
+#                 folder_depth += 1
+#
+#         return folder_depth
+
+
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         folder_depth = 0
-        pattern = r'[a-z0-9\/]'
-        for i in range(len(logs)):
-            if logs[i] == '../' and folder_depth > 0:
-                folder_depth -= 1
-            elif logs[i] == './':
-                continue
 
-            if re.match(pattern, logs[i]):
+        for i in range(len(logs)):
+            if logs[i] == '../':
+                folder_depth = max(0, folder_depth -1)
+            elif logs[i] != './':
                 folder_depth += 1
 
         return folder_depth
