c07deaf1a0fc8c06b3b72768a5807c8fefed35ed
mariami57 <mmitova3@gmail.com>
2025-10-26 21:40:45 +0200

    added additional solution

--- two_pointers/duplicate_zeros.py
+++ two_pointers/duplicate_zeros.py
@@ -1,18 +1,33 @@
 from typing import List
 
 
+# class Solution:
+#     def duplicateZeros(self, arr: List[int]) -> None:
+#         """
+#         Do not return anything, modify arr in-place instead.
+#         """
+#         i = 0
+#         while i < len(arr)-1:
+#             if arr[i] == 0:
+#                 arr.insert(i,0)
+#                 arr.pop()
+#                 i+=1
+#             i+=1
+
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
-        """
-        Do not return anything, modify arr in-place instead.
-        """
-        i = 0
-        while i < len(arr)-1:
+        zeroes = arr.count(0)
+
+        for i in range(len(arr) - 1, -1, -1):
+            if i + zeroes < len(arr):
+                arr[i+zeroes] = arr[i]
             if arr[i] == 0:
-                arr.insert(i,0)
-                arr.pop()
-                i+=1
-            i+=1
+                zeroes -= 1
+                if i+zeroes < len(arr):
+                    arr[i+zeroes] = 0
+        return arr
+
+
 
 
 print(Solution().duplicateZeros(arr = [1,0,2,3,0,4,5,0]))
