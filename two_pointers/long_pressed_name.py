7add6b4dd2b91a771a96ddc5d307219372ed5425
mariami57 <mmitova3@gmail.com>
2025-10-27 21:13:25 +0200

    added more solutions

--- two_pointers/long_pressed_name.py
+++ two_pointers/long_pressed_name.py
@@ -1,15 +1,17 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        i, j = 0, 0
+        i = 0
+        j = 0
 
         while j < len(typed):
             if i < len(name) and name[i] == typed[j]:
-                i += 1
-            elif j == 0 or typed[j] != typed[j - 1]:
+                i+=1
+            elif j == 0 or typed[j]!=typed[j-1]:
                 return False
-            j += 1
-
+            j+=1
         return i == len(name)
 
-
+print(Solution().isLongPressedName(name = "leelee", typed = "lleeelee"))
 print(Solution().isLongPressedName(name ="a", typed ="aaaaaaaaaaaaaaaaa"))
+print(Solution().isLongPressedName(name = "alex", typed = "aaleex"))
+print(Solution().isLongPressedName(name = "saeed", typed = "ssaaedd"))
