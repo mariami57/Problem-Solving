965c916728d8debdb2ec0f6f67d67ebba94874dd
mariami57 <mmitova3@gmail.com>
2025-10-27 20:02:37 +0200

    added more solutions

--- two_pointers/squares_of_a_sorted_array.py
+++ two_pointers/squares_of_a_sorted_array.py
@@ -1,41 +1,21 @@
 from typing import List
 
-
-# class Solution:
-#     def sortedSquares(self, nums: List[int]) -> List[int]:
-#         i, j = 0, len(nums) - 1
-#
-#         while i <= j:
-#             if i == j:
-#                 nums[i] = nums[i] ** 2
-#             else:
-#                 nums[i] = nums[i] ** 2
-#                 nums[j]  = nums[j] ** 2
-#
-#             i += 1
-#             j -= 1
-#
-#
-#         sorted_nums = sorted(nums)
-#
-#         return sorted_nums
-
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        left, right = 0, len(nums) - 1
+        left = 0
+        right = len(nums)-1
         result = [0] * len(nums)
 
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums)-1, -1, -1):
             if abs(nums[left]) > abs(nums[right]):
                 result[i] = nums[left] ** 2
-                left +=1
+                left += 1
             else:
                 result[i] = nums[right] ** 2
-                right -=1
-
+                right -= 1
         return result
 
 
 
-print(Solution().sortedSquares(nums = [-7,-3,2,3,11]))
+print(Solution().sortedSquares(nums = [-5,-3,-2,-1]))
 print(Solution().sortedSquares(nums = [-4,-1,0,3,10]))
