43f4e118d4ba536f84b6ac0476f6e15aeea53b7b
mariami57 <mmitova3@gmail.com>
2025-10-28 09:33:03 +0200

    added more solutions

--- two_pointers/sort_array_by_parity.py
+++ two_pointers/sort_array_by_parity.py
@@ -8,10 +8,14 @@ class Solution:
 
         while i < j:
             if nums[i] % 2 == 0:
-                i+=1
-            elif  nums[j] % 2 != 0:
+                i += 1
+            elif nums[j] % 2 != 0:
                 j-=1
             else:
                 nums[i], nums[j] = nums[j], nums[i]
+
         return nums
+
+print(Solution().sortArrayByParity(nums = [0,1,2]))
 print(Solution().sortArrayByParity(nums = [3,1,2,4]))
+print(Solution().sortArrayByParity(nums = [0,1]))
