547361e672e3f22bf4593dc56d9728a75e67474c
mariami57 <mmitova3@gmail.com>
2025-10-30 21:40:55 +0200

    added more solutions

--- two_pointers/find_the_distance_value_between_two_arrays.py
+++ two_pointers/find_the_distance_value_between_two_arrays.py
@@ -1,24 +1,45 @@
 from typing import List
 
 
+# class Solution:
+#     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+#         i,j, count = 0, 0, 0
+#
+#         while i < len(arr1) and j < len(arr2):
+#             if abs(arr1[i] - arr2[j]) > d:
+#                 j += 1
+#             elif abs(arr1[i] - arr2[j]) <= d:
+#                 j = 0
+#                 i += 1
+#             if j == len(arr2):
+#                 i += 1
+#                 j = 0
+#                 count += 1
+#
+#         return count
+
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        i,j, count = 0, 0, 0
+        i, j, count = 0, 0, 0
 
-        while i < len(arr1) and j < len(arr2):
-            if abs(arr1[i] - arr2[j]) > d:
-                j += 1
-            elif abs(arr1[i] - arr2[j]) <= d:
-                j = 0
-                i += 1
-            if j == len(arr2):
-                i += 1
-                j = 0
-                count += 1
+        arr1.sort()
+        arr2.sort()
 
+        while i <len(arr1) and j < len(arr2):
+            if arr1[i] >= arr2[j]:
+                if abs(arr1[i] - arr2[j]) > d:
+                    j+=1
+                else:
+                    i+=1
+            else:
+                if abs(arr1[i] - arr2[j]) > d:
+                    count+=1
+                i+=1
+        count += len(arr1) - i
         return count
 
-print(Solution().findTheDistanceValue(arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2))
+
+print(Solution().findTheDistanceValue(arr1 = [4,5,8,20,30,40], arr2 = [10,9,1,8], d = 2))
 print(Solution().findTheDistanceValue(arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6))
 print(Solution().findTheDistanceValue(arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3))
 
