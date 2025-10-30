c489095dbaa78acd93f4c31dbbf64e7bc810b824
mariami57 <mmitova3@gmail.com>
2025-10-30 17:52:35 +0200

    added more solutions

--- two_pointers/check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence.py
+++ two_pointers/check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence.py
@@ -1,9 +1,26 @@
+# class Solution:
+#     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+#         words = sentence.split()
+#         for i,word in enumerate(words):
+#             if word.startswith(searchWord):
+#                 return i+1
+#         return -1
+
+
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
         words = sentence.split()
-        for i,word in enumerate(words):
-            if word.startswith(searchWord):
-                return i+1
+        for idx, word in enumerate(words):
+            for i in range(len(word)):
+                if word[i] == searchWord[i]:
+                    i += 1
+                    if i == len(searchWord):
+                        return idx + 1
+                else:
+                    break
+
         return -1
 
-print(Solution().isPrefixOfWord(sentence = "i love eating burger", searchWord = "burg"))
+
+
+print(Solution().isPrefixOfWord(sentence = "b bu bur burg burger", searchWord = "burg"))
