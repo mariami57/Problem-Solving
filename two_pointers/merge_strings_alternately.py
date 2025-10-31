e4a49d34eec5f1c879bd9b7ec88412ac7ad941a8
mariami57 <mmitova3@gmail.com>
2025-10-31 19:48:03 +0200

    added more solutions

---
+++ two_pointers/merge_strings_alternately.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = 0
+        result = []
+
+
+        while i < len(word1) and i < len(word2):
+            result.append(word1[i])
+            result.append(word2[i])
+            i+=1
+
+        if len(word1) < len(word2):
+            result.append(word2[i:])
+        elif len(word1) > len(word2):
+            result.append(word1[i:])
+
+        return ''.join(result)
+
+print(Solution().mergeAlternately(word1 = "abcd", word2 = "pq"))
