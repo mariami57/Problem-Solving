87899196773402c7fc64ef200ed38c66a2467b41
mariami57 <mmitova3@gmail.com>
2025-10-31 20:48:21 +0200

    added more solutions

--- two_pointers/reverse_prefix_of_word.py
+++ two_pointers/reverse_prefix_of_word.py
@@ -1,15 +1,32 @@
+# class Solution:
+#     def reversePrefix(self, word: str, ch: str) -> str:
+#         result = ''
+#
+#         if ch not in word:
+#             return word
+#         else:
+#             idx = word.index(ch)
+#             result+= word[:idx+1][::-1]
+#             result+= word[idx+1:]
+#
+#             return result
+#
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        result = ''
-
         if ch not in word:
             return word
         else:
-            idx = word.index(ch)
-            result+= word[:idx+1][::-1]
-            result+= word[idx+1:]
+            result = []
+            word = list(word)
 
-            return result
+            for i in range(len(word)):
+                if word[i] == ch:
+                    l, r = 0, i
+                    while l < r:
+                        word[l], word[r] = word[r], word[l]
+                        l += 1
+                        r -= 1
+                    return ''.join(word)
 
-
+print(Solution().reversePrefix(word = "abcdefd", ch = "d"))
 print(Solution().reversePrefix(word = "abcd", ch = "z"))
