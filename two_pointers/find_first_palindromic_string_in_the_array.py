cc19d52af5d6185b0aa749b97a96deb18df399ff
mariami57 <mmitova3@gmail.com>
2025-10-31 21:04:13 +0200

    added more solutions

--- two_pointers/find_first_palindromic_string_in_the_array.py
+++ two_pointers/find_first_palindromic_string_in_the_array.py
@@ -8,14 +8,29 @@ from typing import List
 #                 return w
 #         return ''
 #
+# class Solution:
+#     def firstPalindrome(self, words: List[str]) -> str:
+#
+#         for w in words:
+#             if all(w[i]==w[~i] for i in range(len(w)//2)):
+#                 return w
+#         return ''
+
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
 
         for w in words:
-            if all(w[i]==w[~i] for i in range(len(w)//2)):
-                return w
+            l, r =0, len(w) - 1
+            while l <= r:
+                if w[l] == w[r]:
+                    l+=1
+                    r-=1
+                    if l > r:
+                        return w
+                else:
+                    break
         return ''
 
 print(Solution().firstPalindrome(words = ["abc","car","ada","racecar","cool"]))
 print(Solution().firstPalindrome(words = ["notapalindrome","racecar"]))
-print(Solution().firstPalindrome( words = ["def","ghi"]))
+print(Solution().firstPalindrome( words = ["xngla","e","itrn","y","s","pfp","rfd"]))
