6e16bc4a71ded8cbffcaf6c3db180c8d54373557
mariami57 <mmitova3@gmail.com>
2025-11-01 10:08:40 +0200

    added more solutions

--- dfs/binary_tree_inorder_traversal.py
+++ dfs/binary_tree_inorder_traversal.py
@@ -2,6 +2,23 @@
 from typing import Optional, List
 
 
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         result = []
+#
+#         def traverse(curr):
+#             if curr:
+#                 traverse(curr.left)
+#                 result.append(curr.val)
+#                 traverse(curr.right)
+#         traverse(root)
+#         return result
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -9,12 +26,15 @@ class TreeNode:
         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
+        s = []
+        r = []
+        curr = root
 
-        def traverse(curr):
-            if curr:
-                traverse(curr.left)
-                result.append(curr.val)
-                traverse(curr.right)
-        traverse(root)
-        return result
+        while s or curr:
+            while curr:
+                s.append(curr)
+                curr = curr.left
+            curr = s.pop()
+            r.append(curr.val)
+            curr = curr.right
+        return r
