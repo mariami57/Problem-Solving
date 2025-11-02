a55b25e2b81b0f253e27f8066fc0b63bdde8edea
mariami57 <mmitova3@gmail.com>
2025-11-02 20:02:35 +0200

    added more solutions

---
+++ dfs/minimum_depth_of_binary_tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left_depth = self.minDepth(root.left)
+        right_depth = self.minDepth(root.right)
+
+        if left_depth != 0 and right_depth != 0:
+            return 1 + min(left_depth, right_depth)
+        return 1 + max(left_depth, right_depth)
+
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i <len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i +=1
+    return root
+
+if __name__ =='__main__':
+    tree = build_tree([3,9,20,None,None,15,7])
+    tree1 = build_tree([2,None,3,None,4,None,5,None,6])
+    print(Solution().minDepth(tree))
+    print(Solution().minDepth(tree1))
