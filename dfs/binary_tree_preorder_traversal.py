31240a73310970de5ba1d5847af3b593aa61fd08
mariami57 <mmitova3@gmail.com>
2025-11-02 12:11:13 +0200

    added more solutions

--- dfs/binary_tree_preorder_traversal.py
+++ dfs/binary_tree_preorder_traversal.py
@@ -7,18 +7,38 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+# class Solution:
+#     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#
+#         result = []
+#         def preorder(root):
+#             if not root:
+#                 return []
+#
+#             result.append(root.val)
+#             preorder(root.left)
+#             preorder(root.right)
+#             return result
+#
+#         return preorder(root)
+
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 
-        result = []
-        def preorder(root):
-            if not root:
-                return []
+        res = []
 
-            result.append(root.val)
-            preorder(root.left)
-            preorder(root.right)
-            return result
+        if not root:
+            return res
 
-        return preorder(root)
+        st = [root]
+
+        while st:
+            node = st.pop()
+            res.append(node.val)
+
+            if node.right:
+                st.append(node.right)
+            if node.left:
+                st.append(node.left)
+        return res
 
