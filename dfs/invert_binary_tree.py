9a20646ccf00d87bd83026a497483f3702bf3e58
mariami57 <mmitova3@gmail.com>
2025-11-03 18:33:31 +0200

    added more solutions

---
+++ dfs/invert_binary_tree.py
@@ -0,0 +1,52 @@
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i <len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i +=1
+    return root
+
+if __name__ =='__main__':
+    tree = build_tree([4,2,7,1,3,6,9])
+    tree1 = build_tree([2,1,3])
+    tree2 = build_tree([])
+    print(Solution().invertTree(tree))
+    print(Solution().invertTree(tree1))
+    print(Solution().invertTree(tree2))
