a850e0118320651e64e1187ac320169ba7e9f9d2
mariami57 <mmitova3@gmail.com>
2025-11-04 09:43:26 +0200

    added more solutions

--- dfs/binary_tree_preorder_traversal.py
+++ dfs/binary_tree_preorder_traversal.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from collections import deque
 from typing import Optional, List
 
 
@@ -22,23 +23,67 @@ class TreeNode:
 #
 #         return preorder(root)
 
+# class Solution:
+#     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#
+#         res = []
+#
+#         if not root:
+#             return res
+#
+#         st = [root]
+#
+#         while st:
+#             node = st.pop()
+#             res.append(node.val)
+#
+#             if node.right:
+#                 st.append(node.right)
+#             if node.left:
+#                 st.append(node.left)
+#         return res
+
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-
-        res = []
-
         if not root:
-            return res
+            return []
 
-        st = [root]
+        result = []
+        def preorder(node):
+            if not node:
+                return
 
-        while st:
-            node = st.pop()
-            res.append(node.val)
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
 
-            if node.right:
-                st.append(node.right)
-            if node.left:
-                st.append(node.left)
-        return res
+        preorder(root)
+        return result
 
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i <len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i +=1
+    return root
+
+if __name__ =='__main__':
+    tree = build_tree([1,None,2,3])
+    tree1 = build_tree([1,2,3,4,5,None,8,None,None,6,7,9])
+    print(Solution().preorderTraversal(tree))
+    print(Solution().preorderTraversal(tree1))
