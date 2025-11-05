bdc81eaeff1128b5105e766f821419195e4c90d2
mariami57 <mmitova3@gmail.com>
2025-11-05 09:34:21 +0200

    added more solutions

--- dfs/maximum_depth_of_binary_tree.py
+++ dfs/maximum_depth_of_binary_tree.py
@@ -8,14 +8,13 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
         if not root:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
 
 def build_tree(values):
     if not values:
