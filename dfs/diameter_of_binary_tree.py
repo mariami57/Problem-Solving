a0b6fd544c310afa3a2a271ab8cedc5d8bf855a6
mariami57 <mmitova3@gmail.com>
2025-11-08 18:46:54 +0200

    added more solutions

---
+++ dfs/diameter_of_binary_tree.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        self.res = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.res = max(self.res, left + right)
+            return 1 + max(left,right)
+
+        dfs(root)
+        return self.res
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+if __name__ == '__main__':
+    tree = build_tree([1,2,3,4,5])
+    tree1 = build_tree([1,2])
+    print(Solution().diameterOfBinaryTree(tree))
+    print(Solution().diameterOfBinaryTree(tree1))
