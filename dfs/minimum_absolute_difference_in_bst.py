264d0c786ac9b948d76cf221b941255dafdc87d8
mariami57 <mmitova3@gmail.com>
2025-11-08 11:30:51 +0200

    added more solutions

---
+++ dfs/minimum_absolute_difference_in_bst.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.prev = None
+        self.min_diff = float('inf')
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, node.val - self.prev)
+            self.prev = node.val
+            inorder(node.right)
+
+        inorder(root)
+        return self.min_diff
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+if __name__ == '__main__':
+    tree = build_tree([236,104,701,None,227,None,911])
+    tree1 = build_tree([1,0,48,None,None,12,49])
+    print(Solution().getMinimumDifference(tree))
+    print(Solution().getMinimumDifference(tree1))
+
+
+
+
+
