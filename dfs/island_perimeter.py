0c56563c73358c71f6c1503a7e080d96bb1f7fa5
mariami57 <mmitova3@gmail.com>
2025-11-14 09:55:40 +0200

    practiced solutions

--- dfs/island_perimeter.py
+++ dfs/island_perimeter.py
@@ -16,28 +16,28 @@ from typing import List
 #                         perimeter -= 2
 #         return perimeter
 
+
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         perimeter = 0
 
         def dfs(r,c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c]== 0:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
                 return 1
             if grid[r][c] == -1:
                 return 0
+
             grid[r][c] = -1
-            return(
-                    dfs(r+1, c) +
-                    dfs(r-1,c) +
-                    dfs(r,c + 1) +
-                    dfs(r,c-1)
-                   )
+
+            return (dfs(r-1,c) + dfs(r+1,c) + dfs(r,c-1) + dfs(r,c+1))
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     perimeter += dfs(r,c)
         return perimeter
+
+
 
 print(Solution().islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
