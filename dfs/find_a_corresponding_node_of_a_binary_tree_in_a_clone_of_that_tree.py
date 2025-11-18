ea8d97e9f50cb3717e3051dcfc36f64b9b71823f
mariami57 <mmitova3@gmail.com>
2025-11-18 09:02:04 +0200

    added more solutions

--- dfs/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
+++ dfs/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -5,17 +5,28 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# class Solution:
+#     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+#         def dfs(node):
+#             if not node:
+#                 return
+#
+#             if node.val == target.val:
+#                 return node
+#
+#             return dfs(node.left) or dfs(node.right)
+#
+#
+#
+#         return dfs(cloned)
+
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        def dfs(node):
-            if not node:
-                return
+        if not original:
+            return None
 
-            if node.val == target.val:
-                return node
+        if original == target:
+            return cloned
 
-            return dfs(node.left) or dfs(node.right)
-
-
-
-        return dfs(cloned)
+        return (self.getTargetCopy(original.left, cloned.left, target) or
+                self.getTargetCopy(original.right, cloned.right, target))
