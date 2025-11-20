b7d1d36625109f1f60c7d18c17747627e8721cd1
mariami57 <mmitova3@gmail.com>
2025-11-20 19:47:51 +0200

    added more solutions

--- bfs/two_sum_iv_input_is_a_bst.py
+++ bfs/two_sum_iv_input_is_a_bst.py
@@ -15,20 +15,22 @@ class Solution:
             return False
 
         queue = deque([root])
-        diffs = []
+        diffs = set()
 
         while queue:
             node = queue.popleft()
 
-            if k - node.val in diffs:
+            if node.val in diffs:
                 return True
-            diffs.append(node.val)
+            diffs.add(k - node.val)
 
             if node.left:
                 queue.append(node.left)
             if node.right:
                 queue.append(node.right)
         return False
+
+
 
 def build_tree(values):
     if not values:
