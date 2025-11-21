776b3ead5cf633d966647e763ea3056511fcc188
mariami57 <mmitova3@gmail.com>
2025-11-21 20:23:39 +0200

    practiced solutions

--- bfs/maximum_depth_of_binary_tree.py
+++ bfs/maximum_depth_of_binary_tree.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from collections import deque
 from typing import Optional
 
 
@@ -7,22 +8,54 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
 
-        max_depth = 0
-        queue = [root]
-        while queue:
-            max_depth += 1
 
-            for _ in range(len(queue)):
-                node = queue.pop(0)
+        max_depth = 0
+        q = deque([root])
+
+        while q:
+            max_depth += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+
                 if node.left:
-                    queue.append(node.left)
+                    q.append(node.left)
+
                 if node.right:
-                    queue.append(node.right)
+                    q.append(node.right)
         return max_depth
 
 
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+if __name__ == '__main__':
+    tree = build_tree([3,9,20,None,None,15,7])
+    tree1 = build_tree([3,9,20,15,7])
+    print(Solution().maxDepth(tree))
+    print(Solution().maxDepth(tree1))
