84bf3bf0a8912d245d562bb6ed833ad6a4313d0d
mariami57 <mmitova3@gmail.com>
2025-11-21 20:31:54 +0200

    practiced solutions

--- bfs/minimum_depth_of_binary_tree.py
+++ bfs/minimum_depth_of_binary_tree.py
@@ -8,27 +8,81 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#
+#         min_depth = 1
+#
+#         queue = deque()
+#         queue.append(root)
+#
+#         while queue:
+#             node = queue.popleft()
+#
+#
+#             if not node.left and not node.right:
+#                 return min_depth
+#
+#             if node.left:
+#                 queue.append(node.left)
+#
+#             if node.right:
+#                 queue.append(node.right)
+#             min_depth += 1
+#         return min_depth
+
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+
         if not root:
             return 0
 
-        min_depth = 1
+        q = deque([root])
+        min_depth = float('inf')
+        curr_depth = 0
 
-        queue = deque()
-        queue.append(root)
+        while q:
+            curr_depth += 1
 
-        while queue:
-            node = queue.popleft()
+            for _ in range(len(q)):
+                node = q.popleft()
 
+                if not node.left and not node.right:
+                    min_depth = min(min_depth, curr_depth)
 
-            if not node.left and not node.right:
-                return min_depth
+                if node.left:
+                    q.append(node.left)
 
-            if node.left:
-                queue.append(node.left)
-
-            if node.right:
-                queue.append(node.right)
-            min_depth += 1
+                if node.right:
+                    q.append(node.right)
         return min_depth
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+if __name__ == '__main__':
+    tree = build_tree([3,9,20,None,None,15,7])
+    tree1 = build_tree([2,None,3,None,4,None,5,None,6])
+    print(Solution().minDepth(tree))
+    print(Solution().minDepth(tree1))
