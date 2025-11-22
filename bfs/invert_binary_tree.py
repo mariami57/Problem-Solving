e1b31e38afc256d8b115cd3cc7414743156f26e8
mariami57 <mmitova3@gmail.com>
2025-11-22 09:23:19 +0200

    practiced solutions

--- bfs/invert_binary_tree.py
+++ bfs/invert_binary_tree.py
@@ -8,6 +8,19 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+# class Solution:
+#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if not root:
+#             return None
+#
+#         q = deque([root])
+#
+#         while q:
+#             if curr := q.popleft():
+#                 curr.right, curr.left = curr.left, curr.right
+#                 q.extend([curr.left,curr.right])
+#         return root
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
@@ -16,7 +29,46 @@ class Solution:
         q = deque([root])
 
         while q:
-            if curr := q.popleft():
-                curr.right, curr.left = curr.left, curr.right
-                q.extend([curr.left,curr.right])
+            node = q.popleft()
+
+            if not node.left and not node.right:
+                continue
+
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
         return root
+
+def build_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+if __name__ == '__main__':
+    tree = build_tree([4,2,7,1,3,6,9])
+    tree1 = build_tree([2,1,3])
+    print(Solution().invertTree(tree))
+    print(Solution().invertTree(tree1))
+
+
