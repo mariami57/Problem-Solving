1e38a6490f3652a536f17c5f2c2e745f92667684
mariami57 <mmitova3@gmail.com>
2025-11-23 19:35:44 +0200

    added more solutions

--- recursions/find_the_k-th_character_in_string_game_i.py
+++ recursions/find_the_k-th_character_in_string_game_i.py
@@ -1,20 +1,38 @@
+# class Solution:
+#     def kthCharacter(self, k: int) -> str:
+#
+#         def recurse(word):
+#             if k == 1:
+#                 return 'a'
+#             if len(word) >= k:
+#                 return word[k-1]
+#
+#             new_word = word
+#             for ch in word:
+#                 new_word += chr(ord(ch) + 1)
+#
+#             return recurse(new_word)
+#     return recurse('a')
+
 class Solution:
     def kthCharacter(self, k: int) -> str:
-
-        def recurse(word):
+        def helper(k):
             if k == 1:
                 return 'a'
-            if len(word) >= k:
-                return word[k-1]
+            length = 1
 
-            new_word = word
-            for ch in word:
-                new_word += chr(ord(ch) + 1)
+            while 2 * length < k:
+                length *=2
 
-            return recurse(new_word)
+            if k == length or k <= length:
+                return helper(k)
+
+            return chr(ord(helper(k - length)) + 1)
+
+        return helper(k)
 
 
-        return recurse('a')
-print(Solution().kthCharacter(5))
+
+print(Solution().kthCharacter(10))
 
 
