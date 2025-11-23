820a59be00c414e8d3aebc11f6b0e89d6734bbf0
mariami57 <mmitova3@gmail.com>
2025-11-23 18:04:49 +0200

    added more solutions

---
+++ recursions/unique_3-digit_even_numbers.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        result = set()
+        used = [False] * len(digits)
+
+        def backtrack(path):
+            if len(path) == 3:
+                num = path[0] * 100 + path[1] * 10 + path[2]
+                result.add(num)
+                return
+
+            for i in range(len(digits)):
+                if used[i]:
+                    continue
+
+                if len(path) == 0 and digits[i] == 0:
+                    continue
+
+                if len(path) == 2 and digits[i] % 2 != 0:
+                    continue
+
+                used[i] = True
+                backtrack(path + [digits[i]])
+                used[i] = False
+        backtrack([])
+        return len(result)
+
+print(Solution().totalNumbers([1,2,3,4]))
