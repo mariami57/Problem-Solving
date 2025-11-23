c0e55f0b2c0a24308a3a685be988c6c3fa3a8b7f
mariami57 <mmitova3@gmail.com>
2025-11-23 18:22:50 +0200

    added more solutions

--- find_numbers_with_even_number_of_digits.py
+++ find_numbers_with_even_number_of_digits.py
@@ -1,13 +1,24 @@
+import math
 from typing import List
 
+
+# class Solution:
+#     def findNumbers(self, nums: List[int]) -> int:
+#         result = 0
+#         for num in nums:
+#             num_str = str(num)
+#
+#             if len(num_str) % 2 == 0:
+#                 result += 1
+#         return result
 
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         result = 0
         for num in nums:
-            num_str = str(num)
-
-            if len(num_str) % 2 == 0:
+            if num <= 0:
+                continue
+            if (int(math.log10(num)) + 1) % 2 == 0:
                 result += 1
         return result
 
