38f339943d819204f843bde190cedd61ca0028eb
mariami57 <mmitova3@gmail.com>
2025-11-24 12:25:13 +0200

    practiced solutions

--- recursions/palindrome_linked_list.py
+++ recursions/palindrome_linked_list.py
@@ -6,18 +6,23 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
         self.left = ListNode(0, head)
 
-        def recursiveFunc(head):
-
+        def helper(head):
             if not head:
                 return True
-            right = recursiveFunc(head.next)
+
+            right = helper(head.next)
             self.left = self.left.next
+
             return right and self.left.val == head.val
-        return recursiveFunc(head)
+        return helper(head)
+
 
 def build_list(values):
     dummy = ListNode()
@@ -28,6 +33,6 @@ def build_list(values):
     return dummy.next
 
 # Example test
-head = build_list([1, 2])
+head = build_list([2, 1, 1, 1, 2])
 solution = Solution()
 print(solution.isPalindrome(head))
