6e815ee58bf043783e4900ef58049678a7edbb3b
mariami57 <mmitova3@gmail.com>
2025-11-24 09:48:34 +0200

    practiced solutions

--- recursions/remove_linked_list_elements.py
+++ recursions/remove_linked_list_elements.py
@@ -6,34 +6,35 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 # class Solution:
 #     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
 #         if head is None:
 #             return None
 #
-#         head.next = self.removeElements(head.next, val)
+#         temp = ListNode(0)
+#         temp.next = head
+#         prev, curr = temp, head
 #
-#         if head.val == val:
-#             return head.next
+#         while curr:
 #
+#             if curr.val == val:
+#                 prev.next = curr.next
+#             else:
+#                 prev = curr
 #
-#         return head
+#             curr = curr.next
+#         return temp.next
+
 
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head is None:
-            return None
+        if head is None or head.next is None:
+            return head
 
-        temp = ListNode(0)
-        temp.next = head
-        prev, curr = temp, head
+        head.next = self.removeElements(head.next, val)
 
-        while curr:
+        if head.val == val:
+            return head.next
 
-            if curr.val == val:
-                prev.next = curr.next
-            else:
-                prev = curr
-
-            curr = curr.next
-        return temp.next
+        return head
