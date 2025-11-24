4e5b8cf305f165975597ce6192067a1ef5febf98
mariami57 <mmitova3@gmail.com>
2025-11-24 19:12:35 +0200

    practiced solutions

--- recursions/finding_3-digit_even_numbers.py
+++ recursions/finding_3-digit_even_numbers.py
@@ -1,25 +1,50 @@
 from typing import List
 
 
+# class Solution:
+#     def findEvenNumbers(self, digits: List[int]) -> List[int]:
+#         result = set()
+#         used = [False] * len(digits)
+#
+#         def backtrack(path):
+#             if len(path) == 3:
+#                 num = path[0] * 100 + path[1] * 10 + path[2]
+#                 result.add(num)
+#                 return
+#
+#             for i in range(len(digits)):
+#                 if used[i]:
+#                     continue
+#
+#                 if len(path) == 0 and digits[i] == 0:
+#                     continue
+#
+#                 if len(path) == 2 and digits[i] % 2 != 0:
+#                     continue
+#
+#                 used[i] = True
+#                 backtrack(path + [digits[i]])
+#                 used[i] = False
+#         backtrack([])
+#         return sorted(result)
+
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
+
         result = set()
         used = [False] * len(digits)
 
         def backtrack(path):
             if len(path) == 3:
-                num = path[0] * 100 + path[1] * 10 + path[2]
-                result.add(num)
+                result.add(path[0] * 100 + path[1] * 10 + path[2])
                 return
 
             for i in range(len(digits)):
                 if used[i]:
                     continue
-
-                if len(path) == 0 and digits[i] == 0:
+                if digits[i] == 0 and len(path) == 0:
                     continue
-
-                if len(path) == 2 and digits[i] % 2 != 0:
+                if digits[i] % 2 != 0 and len(path) == 2:
                     continue
 
                 used[i] = True
