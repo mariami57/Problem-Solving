1ed4dd04f3a0909b9b026af296e00a3afa43b224
mariami57 <mmitova3@gmail.com>
2025-11-25 09:14:27 +0200

    practiced solutions

--- recursions/merge_two_sorted_lists.py
+++ recursions/merge_two_sorted_lists.py
@@ -9,19 +9,18 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
+        if not list1 and not list2:
             return None
 
-        if list1 is None:
+        if not list1:
             return list2
 
-        if list2 is None:
+        if not list2:
             return list1
 
         if list1.val < list2.val:
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
-
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
--- recursions/remove_linked_list_elements.py
+++ recursions/remove_linked_list_elements.py
@@ -27,12 +27,13 @@ class ListNode:
 #         return temp.next
 
 
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head is None or head.next is None:
+        if head is None:
             return head
 
-        head.next = self.removeElements(head.next, val)
+        head.next = self.removeElements(head.next,val)
 
         if head.val == val:
             return head.next
