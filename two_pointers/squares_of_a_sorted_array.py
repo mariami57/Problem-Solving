f00bdbf8eb234f1b61a391fffc19505d77dedebe
mariami57 <mmitova3@gmail.com>
2025-11-25 19:35:39 +0200

    practiced solutions

--- two_pointers/squares_of_a_sorted_array.py
+++ two_pointers/squares_of_a_sorted_array.py
@@ -3,19 +3,24 @@ from typing import List
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         left = 0
-        right = len(nums)-1
+        right = len(nums) - 1
+
         result = [0] * len(nums)
 
-        for i in range(len(nums)-1, -1, -1):
+        pos = len(nums) - 1
+
+        while left <= right:
             if abs(nums[left]) > abs(nums[right]):
-                result[i] = nums[left] ** 2
+                result[pos] = nums[left] ** 2
                 left += 1
             else:
-                result[i] = nums[right] ** 2
+                result[pos] = nums[right] ** 2
                 right -= 1
+            pos -= 1
+
+
         return result
-
-
 
 print(Solution().sortedSquares(nums = [-5,-3,-2,-1]))
 print(Solution().sortedSquares(nums = [-4,-1,0,3,10]))
+
