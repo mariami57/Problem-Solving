07ec366962b1a0cde9a243ceb8f38b81160cdc5c
mariami57 <mmitova3@gmail.com>
2025-11-25 12:52:58 +0200

    practiced solutions

---
+++ recursions/remove_duplicates_from_sorted_list.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        head.next = self.deleteDuplicates(head.next)
+
+
+        if head.val == head.next.val:
+            head = head.next
+
+        return head
+
+
+
+
+def build_list(values):
+    dummy = ListNode()
+    cur = dummy
+    for v in values:
+        cur.next = ListNode(v)
+        cur = cur.next
+    return dummy.next
+
+
+def linked_list_to_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+# Example test
+head = build_list([1,1,2,3,3])
+solution = Solution()
+print(linked_list_to_list(solution.deleteDuplicates(head)))
