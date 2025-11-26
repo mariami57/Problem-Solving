ae59a8a8b6333c511d744021dae771ec07a455cb
mariami57 <mmitova3@gmail.com>
2025-11-26 13:14:49 +0200

    added more solutions

--- sorting/third_maximum_number.py
+++ sorting/third_maximum_number.py
@@ -1,19 +1,25 @@
 from typing import List
 
 
+# class Solution:
+#     def thirdMax(self, nums: List[int]) -> int:
+#         max_nums = {}
+#
+#         for num in nums:
+#             max_nums[num] = max_nums.get(num, 0) + 1
+#         #
+#         max_sorted = sorted(max_nums.items(), key=lambda x: -x[0])
+#
+#         if len(max_sorted) >= 3:
+#             return max_sorted[2][0]
+#         else:
+#             return max(max_nums, key = lambda x: x)
+
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        max_nums = {}
+        nums = sorted(list(set(nums)))
 
-        for num in nums:
-            max_nums[num] = max_nums.get(num, 0) + 1
-        #
-        max_sorted = sorted(max_nums.items(), key=lambda x: -x[0])
-
-        if len(max_sorted) >= 3:
-            return max_sorted[2][0]
-        else:
-            return max(max_nums, key = lambda x: x)
+        return nums[-1] if len(nums) < 3 else nums[-3]
 
 
 print(Solution().thirdMax([1,2,2,5,3,5]))
