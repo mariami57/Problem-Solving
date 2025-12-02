17dc561ab493fa5eb2c04c989fca5abfaca3acaf
mariami57 <mmitova3@gmail.com>
2025-12-02 09:29:49 +0200

    added more solutions

---
+++ sorting/array_partition.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+
+        for i in range(0, len(nums), 2):
+            res += nums[i]
+        return res
+
+print(Solution().arrayPairSum([6,2,6,5,1,2]))
