a833b19d3867f67e1621bf22d1114e58c4437be5
mariami57 <mmitova3@gmail.com>
2025-12-04 17:51:06 +0200

    added more solutions

---
+++ sorting/the_k_weakest_rows_in_a_matrix.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        sold_count = {}
+        result = []
+        for i in range(len(mat)):
+            sold_count[i] = mat[i].count(1)
+
+        sorted_count = sorted(sold_count.items(), key=lambda x: x[1])
+
+
+        for key,v in sorted_count:
+            if len(result) < k:
+                result.append(key)
+        return result
+
+
+
+print(Solution().kWeakestRows(mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3))
+
+print(Solution().kWeakestRows(mat =
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]],
+k = 2))
