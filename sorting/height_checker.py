c12fe8c7e39a8a91e7c07c1e93783f1e93c0268a
mariami57 <mmitova3@gmail.com>
2025-12-04 16:08:25 +0200

    added more solutions

---
+++ sorting/height_checker.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = heights.copy()
+        expected.sort()
+        res = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                res += 1
+        return res
+
+print(Solution().heightChecker([1,1,4,2,1,3]))
+print(Solution().heightChecker([5,1,2,3,4]))
+print(Solution().heightChecker([1,2,3,4,5]))
