eadadbb40db9c65e50c72faa227dd2b9888d5329
mariami57 <mmitova3@gmail.com>
2025-12-06 20:09:19 +0200

    added more solutions

---
+++ single_number.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(0,len(nums)-1,2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+        return nums[-1]
+
+print(Solution().singleNumber([4,1,2,1,2]))
+print(Solution().singleNumber([2,2,1]))
+print(Solution().singleNumber([1]))
+
+
