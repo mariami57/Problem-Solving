a6f2030f10238806a6aaac08865012e403823c5f
mariami57 <mmitova3@gmail.com>
2025-12-06 18:54:45 +0200

    added more solutions

--- pascal's_triangle_ii.py
+++ pascal's_triangle_ii.py
@@ -1,18 +1,25 @@
 from typing import List
 
+#
+# class Solution:
+#     def getRow(self, rowIndex: int) -> List[int]:
+#         p_triangle = [[1]]
+#
+#         for j in range(rowIndex + 1):
+#             dummy_row = [0] + p_triangle[-1] + [0]
+#             row = []
+#             for i in range(len(p_triangle[-1]) + 1):
+#                 row.append(dummy_row[i] + dummy_row[i+1])
+#             p_triangle.append(row)
+#
+#
+#         return p_triangle[rowIndex]
 
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        p_triangle = [[1]]
-
-        for j in range(rowIndex + 1):
-            dummy_row = [0] + p_triangle[-1] + [0]
-            row = []
-            for i in range(len(p_triangle[-1]) + 1):
-                row.append(dummy_row[i] + dummy_row[i+1])
-            p_triangle.append(row)
-
-
-        return p_triangle[rowIndex]
+        row = [1]
+        for _ in range(rowIndex):
+            row = [left + right for left, right in zip([0]+row, row+[0])]
+        return row
 
 print(Solution().getRow(3))
