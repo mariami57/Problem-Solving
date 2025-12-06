e0ca5c0078bedb82a24736a5e55a14177431c8aa
mariami57 <mmitova3@gmail.com>
2025-12-06 20:33:31 +0200

    added more solutions

--- find_the_xor_of_numbers_which_appear_twice.py
+++ find_the_xor_of_numbers_which_appear_twice.py
@@ -1,17 +1,29 @@
 from typing import List
 
 
+# class Solution:
+#     def duplicateNumbersXOR(self, nums: List[int]) -> int:
+#         result = 0
+#         counts = {}
+#         for num in nums:
+#             counts[num] = counts.get(num, 0) + 1
+#
+#         for k, v in counts.items():
+#             if v == 2:
+#                 result ^= k
+#         return result
+
 class Solution:
     def duplicateNumbersXOR(self, nums: List[int]) -> int:
-        result = 0
-        counts = {}
-        for num in nums:
-            counts[num] = counts.get(num, 0) + 1
+        res = 0
+        seen = set()
 
-        for k, v in counts.items():
-            if v == 2:
-                result ^= k
-        return result
+        for num in nums:
+            if num in seen:
+                res ^= num
+            else:
+                seen.add(num)
+        return res
 
 print(Solution().duplicateNumbersXOR([1,2,1,3]))
 print(Solution().duplicateNumbersXOR([1,2,3]))
