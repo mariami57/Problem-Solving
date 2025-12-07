31bb2501e80c5ba7f96a394d508108e4df608afd
mariami57 <mmitova3@gmail.com>
2025-12-07 19:42:33 +0200

    added more solutions

---
+++ isomorphic_strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_index_s = {}
+        char_index_t = {}
+
+        for i in range(len(s)):
+            if s[i] not in char_index_s:
+                char_index_s[s[i]] = i
+            if t[i] not in char_index_t:
+                char_index_t[t[i]] = i
+
+            if char_index_s[s[i]] != char_index_t[t[i]]:
+                return False
+        return True
+
+print(Solution().isIsomorphic(s = "foo", t = "bar"))
+print(Solution().isIsomorphic(s = "badc", t = "baba"))
+print(Solution().isIsomorphic(s = "egg", t = "add"))
+
