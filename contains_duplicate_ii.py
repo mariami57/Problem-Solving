433d263813af857f0500b9d2895b5e5e50948225
mariami57 <mmitova3@gmail.com>
2025-12-07 20:26:08 +0200

    added more solutions

---
+++ contains_duplicate_ii.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        indexes = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in indexes:
+                indexes[nums[i]] = [i]
+            else:
+                indexes[nums[i]].append(i)
+
+        for val in indexes.values():
+            for j in range(len(val) - 1):
+                if abs(val[j] - val[(j + 1)]) <= k:
+                    return True
+        return False
+
+print(Solution().containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2))
+print(Solution().containsNearbyDuplicate(nums = [1,2,3,1], k = 3))
+print(Solution().containsNearbyDuplicate(nums = [1,0,1,1], k = 1))
+
+
--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,16 +1,31 @@
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         char_index_s = {}
+#         char_index_t = {}
+#
+#         for i in range(len(s)):
+#             if s[i] not in char_index_s:
+#                 char_index_s[s[i]] = i
+#             if t[i] not in char_index_t:
+#                 char_index_t[t[i]] = i
+#
+#             if char_index_s[s[i]] != char_index_t[t[i]]:
+#                 return False
+#         return True
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        char_index_s = {}
-        char_index_t = {}
+        mapped = {}
 
-        for i in range(len(s)):
-            if s[i] not in char_index_s:
-                char_index_s[s[i]] = i
-            if t[i] not in char_index_t:
-                char_index_t[t[i]] = i
-
-            if char_index_s[s[i]] != char_index_t[t[i]]:
+        for sc, tc in zip(s,t):
+            if sc in mapped:
+                if mapped[sc] != tc:
+                    return False
+            elif tc in mapped.values():
                 return False
+
+            mapped[sc] = tc
+
         return True
 
 print(Solution().isIsomorphic(s = "foo", t = "bar"))
