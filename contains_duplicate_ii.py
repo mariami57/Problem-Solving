9f1049a3388eb5d22ab36d2bcd1a8463a9473b9d
mariami57 <mmitova3@gmail.com>
2025-12-07 20:29:51 +0200

    added more solutions

--- contains_duplicate_ii.py
+++ contains_duplicate_ii.py
@@ -1,22 +1,32 @@
 from typing import List
 
 
+# class Solution:
+#     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+#         indexes = {}
+#
+#         for i in range(len(nums)):
+#             if nums[i] not in indexes:
+#                 indexes[nums[i]] = [i]
+#             else:
+#                 indexes[nums[i]].append(i)
+#
+#         for val in indexes.values():
+#             for j in range(len(val) - 1):
+#                 if abs(val[j] - val[(j + 1)]) <= k:
+#                     return True
+#         return False
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        indexes = {}
+        seen = {}
 
-        for i in range(len(nums)):
-            if nums[i] not in indexes:
-                indexes[nums[i]] = [i]
+        for i, val in enumerate(nums):
+            if val in seen and i - seen[val] <= k:
+                return True
             else:
-                indexes[nums[i]].append(i)
-
-        for val in indexes.values():
-            for j in range(len(val) - 1):
-                if abs(val[j] - val[(j + 1)]) <= k:
-                    return True
+                seen[val] = i
         return False
-
 print(Solution().containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2))
 print(Solution().containsNearbyDuplicate(nums = [1,2,3,1], k = 3))
 print(Solution().containsNearbyDuplicate(nums = [1,0,1,1], k = 1))
