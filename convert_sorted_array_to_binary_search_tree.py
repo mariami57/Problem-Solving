e2facc75400928ee3e303fae95100bad9daa9f06
mariami57 <mmitova3@gmail.com>
2025-12-07 16:29:06 +0200

    added more solutions

--- convert_sorted_array_to_binary_search_tree.py
+++ convert_sorted_array_to_binary_search_tree.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from collections import deque
 from typing import List, Optional
 
 
@@ -7,21 +8,48 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+# class Solution:
+#     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+#         if not nums:
+#             return None
+#         def convert(left, right):
+#             if left > right:
+#                 return
+#
+#             mid = (left + right)  // 2
+#             node = TreeNode(nums[mid])
+#
+#             node.left = convert(left, mid -1)
+#             node.right = convert(mid+1, right)
+#
+#             return node
+#         return convert(0, len(nums)-1)
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
         if not nums:
             return None
-        def convert(left, right):
-            if left > right:
-                return
 
-            mid = (left + right)  // 2
-            node = TreeNode(nums[mid])
+        mid = (len(nums) -1) // 2
+        root = TreeNode(nums[mid])
+        queue = deque([(root, 0, len(nums) - 1)])
 
-            node.left = convert(left, mid -1)
-            node.right = convert(mid+1, right)
+        while queue:
+            node, left, right = queue.popleft()
 
-            return node
-        return convert(0, len(nums)-1)
+            mid = (left + right) // 2
 
+            if left <= mid-1:
+                left_mid = (left + mid - 1) // 2
+                node.left = TreeNode(nums[left_mid])
+                queue.append((node.left, left, mid -1))
+
+            if right >= mid + 1:
+                right_mid = (mid + 1 + right) // 2
+                node.right = TreeNode(nums[right_mid])
+                queue.append((node.right, mid + 1, right))
+
+        return root
+print(Solution().sortedArrayToBST([1,3]))
 print(Solution().sortedArrayToBST([-10,-3,0,5,9]))
