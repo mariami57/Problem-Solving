04a4279dccc210b8535fc034ec7cde6819b0c426
mariami57 <mmitova3@gmail.com>
2025-12-07 20:35:50 +0200

    added more solutions

--- contains_duplicate_ii.py
+++ contains_duplicate_ii.py
@@ -17,16 +17,31 @@ from typing import List
 #                     return True
 #         return False
 
+# class Solution:
+#     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+#         seen = {}
+#
+#         for i, val in enumerate(nums):
+#             if val in seen and i - seen[val] <= k:
+#                 return True
+#             else:
+#                 seen[val] = i
+#         return False
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}
+        seen = set()
 
         for i, val in enumerate(nums):
-            if val in seen and i - seen[val] <= k:
+            if i > k:
+                seen.remove(nums[i - k - 1])
+
+            if val in seen:
                 return True
-            else:
-                seen[val] = i
+
+            seen.add(val)
         return False
+
 print(Solution().containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2))
 print(Solution().containsNearbyDuplicate(nums = [1,2,3,1], k = 3))
 print(Solution().containsNearbyDuplicate(nums = [1,0,1,1], k = 1))
