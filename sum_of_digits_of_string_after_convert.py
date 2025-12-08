6a4d2898ea7e0349a0086213097941e814ce3d24
mariami57 <mmitova3@gmail.com>
2025-12-08 16:57:54 +0200

    added more solutions

---
+++ sum_of_digits_of_string_after_convert.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        num = ''
+        for chr in s:
+            num += str(ord(chr) - 97 + 1)
+        num = int(num)
+        sum = 0
+
+        i = 0
+
+        while i <= k-1:
+            sum = 0
+            while num > 0:
+                dig = num % 10
+                sum += dig
+                num //= 10
+            num = sum
+            i +=1
+        return sum
+print(Solution().getLucky(s = "iiii", k = 1))
+print(Solution().getLucky(s = "zbax", k = 2))
+print(Solution().getLucky(s = "leetcode", k = 2))
