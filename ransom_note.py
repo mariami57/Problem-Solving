4533649980d91fd7a84a4e68f20648addfc13f89
mariami57 <mmitova3@gmail.com>
2025-12-09 20:19:57 +0200

    added more solutions

--- ransom_note.py
+++ ransom_note.py
@@ -1,20 +1,40 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(magazine) < len(ransomNote):
-            return False
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         if len(magazine) < len(ransomNote):
+#             return False
+#
+#         used = []
+#
+#         for char in ransomNote:
+#             if ransomNote.count(char) > magazine.count(char):
+#                 return False
+#             if char not in magazine:
+#                 return False
+#             used.append(char)
+#
+#
+#         return True
+from collections import Counter
 
-        used = []
 
-        for char in ransomNote:
-            if ransomNote.count(char) > magazine.count(char):
-                return False
-            if char not in magazine:
-                return False
-            used.append(char)
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         maga_hash = {}
+#
+#         for c in magazine:
+#             maga_hash[c] = 1 + maga_hash.get(c, 0)
+#
+#         for c in ransomNote:
+#             if c not in maga_hash or maga_hash[c] <= 0:
+#                 return False
+#             maga_hash[c] -= 1
+#
+#         return True
 
-
-        return True
-
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        return st1 & st2 == st1
 
 print(Solution().canConstruct(ransomNote = "fffbfg", magazine = "effjfggbffjdgbjjhhdegh"))
 print(Solution().canConstruct(ransomNote = "a", magazine = "b"))
