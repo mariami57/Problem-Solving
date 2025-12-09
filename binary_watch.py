6bea3ec8e5b71de5a54fcd243de9ea9984928285
mariami57 <mmitova3@gmail.com>
2025-12-09 20:48:36 +0200

    added more solutions

---
+++ binary_watch.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        times = []
+
+        for h in range(12):
+            for m in range(60):
+                h_ones = bin(h).count('1')
+                m_ones = bin(m).count('1')
+
+                if h_ones + m_ones == turnedOn:
+                    times.append(f"{h}:{m:02d}")
+        return times
+
+print(Solution().readBinaryWatch(1))
