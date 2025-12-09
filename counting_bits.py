d9336aed3937470c3e93a5deb2f5daa106d15d84
mariami57 <mmitova3@gmail.com>
2025-12-09 19:59:17 +0200

    added more solutions

--- counting_bits.py
+++ counting_bits.py
@@ -1,17 +1,30 @@
 from typing import List
 
 
+# class Solution:
+#     def countBits(self, n: int) -> List[int]:
+#         result = []
+#
+#
+#
+#         for i in range(n+1):
+#             bin_dig = str(bin(i)[2:])
+#             result.append(bin_dig.count('1'))
+#
+#         return result
+
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        result = []
+        dp = [0] * (n+1)
+        sub = 1
 
+        for i in range(1, n+1):
+            if sub * 2 == i:
+                sub = i
 
+            dp[i] = dp[i - sub] + 1
 
-        for i in range(n+1):
-            bin_dig = str(bin(i)[2:])
-            result.append(bin_dig.count('1'))
+        return dp
 
-        return result
-
-print(Solution().countBits(2))
 print(Solution().countBits(5))
+print(Solution().countBits(2))
