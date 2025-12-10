ff15beb399d1a67f23e2f6cb066e7a2a65fa0337
mariami57 <mmitova3@gmail.com>
2025-12-10 16:46:05 +0200

    added more solutions

--- number_of_segments_in_a_string.py
+++ number_of_segments_in_a_string.py
@@ -1,20 +1,30 @@
+# class Solution:
+#     def countSegments(self, s: str) -> int:
+#         if s == 0:
+#             return 0
+#
+#         start, end = 0, 0
+#         segments = 0
+#
+#         while start < len(s) and end < len(s):
+#             while s[end] != ' ' and end <= len(s)-2:
+#                 end += 1
+#             if s[start:end+1] != ' ':
+#                 segments += 1
+#             start, end = end + 1, end + 1
+#
+#         return segments
+
 class Solution:
     def countSegments(self, s: str) -> int:
         if s == 0:
             return 0
 
-        start, end = 0, 0
-        segments = 0
+        s = s.split()
 
-        while start < len(s) and end < len(s):
-            while s[end] != ' ' and end <= len(s)-2:
-                end += 1
-            if s[start:end+1] != ' ':
-                segments += 1
-            start, end = end + 1, end + 1
+        return len(s)
 
-        return segments
-
+print(Solution().countSegments(s = "Of all the gin joints in all the towns in all the world,   "))
 print(Solution().countSegments(s = "                "))
 print(Solution().countSegments(s = "Hello, my name is John"))
 print(Solution().countSegments(s = "Hello"))
