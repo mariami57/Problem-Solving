483baa5d204660726c4041d506ce8e4576a556a3
mariami57 <mmitova3@gmail.com>
2025-12-11 20:49:13 +0200

    practiced solutions

--- bit_manipulation/convert_a_number_to_hexadecimal.py
+++ bit_manipulation/convert_a_number_to_hexadecimal.py
@@ -1,21 +1,43 @@
+# class Solution:
+#     def toHex(self, num: int) -> str:
+#         if num == 0:
+#             return '0'
+#
+#         if num < 0:
+#             num = ( 1 << 32) + num
+#
+#         hex_digits ='0123456789abcdef'
+#         hex_num = ''
+#
+#         while num > 0:
+#             digit = num % 16
+#             hex_digit = hex_digits[digit]
+#             hex_num = hex_digit + hex_num
+#             num //= 16
+#
+#         return hex_num
+
+
 class Solution:
     def toHex(self, num: int) -> str:
         if num == 0:
             return '0'
 
+        res = ''
+
         if num < 0:
             num = ( 1 << 32) + num
 
-        hex_digits ='0123456789abcdef'
-        hex_num = ''
+        hex_digits= '0123456789abcdef'
 
         while num > 0:
             digit = num % 16
             hex_digit = hex_digits[digit]
-            hex_num = hex_digit + hex_num
+            res = hex_digit + res
             num //= 16
 
-        return hex_num
+        return res
+
 
 print(Solution().toHex(-1))
 print(Solution().toHex(26))
