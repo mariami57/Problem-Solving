9ad43f7c9faf28a8b2005f9c6628f1250df0450c
mariami57 <mmitova3@gmail.com>
2025-12-12 12:55:36 +0200

    added more solutions

--- bit_manipulation/number_complement.py
+++ bit_manipulation/number_complement.py
@@ -1,15 +1,23 @@
+# class Solution:
+#     def findComplement(self, num: int) -> int:
+#         bit_num = bin(num)
+#         complement = ''
+#
+#         for char in bit_num[2:]:
+#             if char == '1':
+#                 complement += '0'
+#             else:
+#                 complement += '1'
+#
+#         return int(complement, 2)
+
 class Solution:
     def findComplement(self, num: int) -> int:
-        bit_num = bin(num)
-        complement = ''
+        bit_length = num.bit_length()
 
-        for char in bit_num[2:]:
-            if char == '1':
-                complement += '0'
-            else:
-                complement += '1'
+        dummy = (1 << bit_length) - 1
 
-        return int(complement, 2)
+        return num ^ dummy
 
 print(Solution().findComplement(5))
 print(Solution().findComplement(1))
