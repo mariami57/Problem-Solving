63bbadbb3103072b084c4c4204246088c813ce34
mariami57 <mmitova3@gmail.com>
2025-12-12 12:11:52 +0200

    practiced solutions

--- add_strings.py
+++ add_strings.py
@@ -1,8 +1,9 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        i, j = len(num1) -1, len(num2) - 1
-        carry = 0
         result = []
+        i = len(num1) - 1
+        j = len(num2) - 1
+        carry = 0
 
         while i >= 0 or j >= 0 or carry:
             digit1 = int(num1[i]) if i >= 0 else 0
@@ -19,4 +20,6 @@ class Solution:
 
         return ''.join((result[::-1]))
 
+
+print(Solution().addStrings(num1 = "456", num2 = "77"))
 print(Solution().addStrings(num1 = "11", num2 = "123"))
