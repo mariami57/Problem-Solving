a96d51c1d87ffffc349487e72c98527d8bb518e0
mariami57 <mmitova3@gmail.com>
2025-12-13 17:55:18 +0200

    added more solutions

---
+++ detect_capital.py
@@ -0,0 +1,10 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+
+        if word == word.upper() or (word[0] == word[0].upper() and word[1:] == word[1:].lower()) or word == word.lower():
+            return True
+        return False
+
+print(Solution().detectCapitalUse(word = "Leetcode"))
+print(Solution().detectCapitalUse(word = "USA"))
+print(Solution().detectCapitalUse(word = "FlaG"))
