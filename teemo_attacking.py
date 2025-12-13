251b66335294811035e7f46e62eb8c9b123e3bd7
mariami57 <mmitova3@gmail.com>
2025-12-13 13:20:39 +0200

    added more solutions

---
+++ teemo_attacking.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        result = 0
+
+        for i in range(len(timeSeries)-1):
+            diff = timeSeries[i+1] - timeSeries[i]
+            result += min(diff, duration)
+
+        result += duration
+        return result
+
+print(Solution().findPoisonedDuration(timeSeries = [1,4], duration = 2))
+print(Solution().findPoisonedDuration(timeSeries = [1,2], duration = 2))
+print(Solution().findPoisonedDuration(timeSeries = [1,2,3,4,5], duration = 5))
+
