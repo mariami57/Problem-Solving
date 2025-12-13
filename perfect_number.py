7415582bd325971af58b05884028d80b37613637
mariami57 <mmitova3@gmail.com>
2025-12-13 17:47:12 +0200

    added more solutions

---
+++ perfect_number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+
+        total_sum = 1
+
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                total_sum += i
+                if i != num // i:
+                    total_sum += num // i
+
+        return total_sum == num
+print(Solution().checkPerfectNumber(28))
+print(Solution().checkPerfectNumber(7))
