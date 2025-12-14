d0c615e96b04e43df75dece47a36de7ddc4fa10e
mariami57 <mmitova3@gmail.com>
2025-12-14 10:48:51 +0200

    practiced solutions

--- detect_capital.py
+++ detect_capital.py
@@ -1,7 +1,13 @@
+# class Solution:
+#     def detectCapitalUse(self, word: str) -> bool:
+#
+#         if word == word.upper() or (word[0] == word[0].upper() and word[1:] == word[1:].lower()) or word == word.lower():
+#             return True
+#         return False
+
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-
-        if word == word.upper() or (word[0] == word[0].upper() and word[1:] == word[1:].lower()) or word == word.lower():
+        if word.isupper() or word.islower() or (word[0].isupper() and word[1:].islower()):
             return True
         return False
 
