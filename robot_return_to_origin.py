d5d747c4d2dd489fecc7c524cdc554b1e1398e4a
mariami57 <mmitova3@gmail.com>
2025-12-15 13:10:12 +0200

    added more solutions

--- robot_return_to_origin.py
+++ robot_return_to_origin.py
@@ -1,20 +1,24 @@
+# class Solution:
+#     def judgeCircle(self, moves: str) -> bool:
+#         start_pos, cur_pos = [0, 0], [0, 0]
+#         directions = {
+#             'U': (-1, 0),
+#             'D': (1,0),
+#             'L': (0,-1),
+#             'R': (0,1)
+#         }
+#
+#         for move in moves:
+#             r, c = directions[move]
+#             cur_pos = [cur_pos[0] + r, cur_pos[1]+c]
+#
+#         if cur_pos == start_pos:
+#             return True
+#         return False
+
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        start_pos, cur_pos = [0, 0], [0, 0]
-        directions = {
-            'U': (-1, 0),
-            'D': (1,0),
-            'L': (0,-1),
-            'R': (0,1)
-        }
-
-        for move in moves:
-            r, c = directions[move]
-            cur_pos = [cur_pos[0] + r, cur_pos[1]+c]
-
-        if cur_pos == start_pos:
-            return True
-        return False
+        return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
 
 print(Solution().judgeCircle("UD"))
 print(Solution().judgeCircle("LL"))
