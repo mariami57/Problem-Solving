3daa93b93c2a303fc2ef4a76f21ed783189f95d3
mariami57 <mmitova3@gmail.com>
2025-12-16 12:03:36 +0200

    added more solutions

--- kth_largest_element_in_a_stream.py
+++ kth_largest_element_in_a_stream.py
@@ -1,14 +1,32 @@
 from typing import List
+import heapq
 
+# class KthLargest:
+#
+#     def __init__(self, k: int, nums: List[int]):
+#         self.k = k
+#         self.nums = nums
+#
+#     def add(self, val: int) -> int:
+#         self.nums.append(val)
+#         self.nums.sort(reverse=True)
+#
+#         return self.nums[self.k-1]
 
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
+        heapq.heapify(self.nums)
+
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums.sort(reverse=True)
+        heapq.heappush(self.nums, val)
 
-        return self.nums[self.k-1]
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+
+        return self.nums[0]
