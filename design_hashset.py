b9796c68fdd8ceaf18f88087ab000375688241be
mariami57 <mmitova3@gmail.com>
2025-12-16 12:18:07 +0200

    added more solutions

--- design_hashset.py
+++ design_hashset.py
@@ -1,19 +1,32 @@
+# class MyHashSet:
+#
+#     def __init__(self):
+#         self.my_set = []
+#
+#     def add(self, key: int) -> None:
+#         if key not in self.my_set:
+#             self.my_set.append(key)
+#
+#     def remove(self, key: int) -> None:
+#         if key in self.my_set:
+#             self.my_set.remove(key)
+#
+#     def contains(self, key: int) -> bool:
+#         if key in self.my_set:
+#             return True
+#         else:
+#             return False
+
 class MyHashSet:
 
     def __init__(self):
-        self.my_set = []
+        self.arr = [False] * 1000001
 
     def add(self, key: int) -> None:
-        if key not in self.my_set:
-            self.my_set.append(key)
+        self.arr[key] = True
 
     def remove(self, key: int) -> None:
-        if key in self.my_set:
-            self.my_set.remove(key)
+        self.arr[key] = False
 
     def contains(self, key: int) -> bool:
-        if key in self.my_set:
-            return True
-        else:
-            return False
-
+        return self.arr[key]
