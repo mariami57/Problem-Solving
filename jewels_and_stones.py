d093089a6a71eb4a80797d7d4e64127a5916ea10
mariami57 <mmitova3@gmail.com>
2025-12-17 18:51:52 +0200

    added more solutions

--- jewels_and_stones.py
+++ jewels_and_stones.py
@@ -1,10 +1,14 @@
+# class Solution:
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         count = 0
+#
+#         for char in jewels:
+#             count += stones.count(char)
+#         return count
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        count = 0
-
-        for char in jewels:
-            count += stones.count(char)
-        return count
+        return sum(i in jewels for i in stones)
 
 print(Solution().numJewelsInStones(jewels = "aA", stones = "aAAbbbb"))
 print(Solution().numJewelsInStones(jewels = "z", stones = "ZZ"))
