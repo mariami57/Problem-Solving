e0b967235db2a798a4952b2035c75984b3286de7
mariami57 <mmitova3@gmail.com>
2025-12-17 09:46:45 +0200

    added more solutions

--- find_pivot_index.py
+++ find_pivot_index.py
@@ -1,13 +1,28 @@
 from typing import List
 
 
+# class Solution:
+#     def pivotIndex(self, nums: List[int]) -> int:
+#         for i in range(len(nums)):
+#             left_sum = sum(nums[:i])
+#             right_sum = sum(nums[i+1:])
+#             if left_sum == right_sum:
+#                 return i
+#         return -1
+
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_total = 0
+
         for i in range(len(nums)):
-            left_sum = sum(nums[:i])
-            right_sum = sum(nums[i+1:])
-            if left_sum == right_sum:
+            right_total = total - left_total - nums[i]
+
+            if right_total == left_total:
                 return i
+
+            left_total += nums[i]
         return -1
 
 print(Solution().pivotIndex([2,1,-1]))
