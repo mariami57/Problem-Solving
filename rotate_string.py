d03082fc68d301786d5def463745c203cfe1a879
mariami57 <mmitova3@gmail.com>
2025-12-18 09:01:15 +0200

    added more solutions

--- rotate_string.py
+++ rotate_string.py
@@ -1,14 +1,20 @@
+# class Solution:
+#     def rotateString(self, s: str, goal: str) -> bool:
+#         i = 0
+#
+#         while i < len(s):
+#             first_char = s[0]
+#             rest = s[1:]
+#             s = rest + first_char
+#             if s == goal:
+#                 return True
+#             i += 1
+#         return False
+
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        i = 0
-
-        while i < len(s):
-            first_char = s[0]
-            rest = s[1:]
-            s = rest + first_char
-            if s == goal:
-                return True
-            i += 1
-        return False
+        if len(s) != len(goal):
+            return False
+        return goal in s + s
 print(Solution().rotateString(s = "abcde", goal = "cdeab"))
 print(Solution().rotateString(s = "abcde", goal = "abced"))
