c75a5bf354dc5d58ebb7f16e225cd1f23d02b746
mariami57 <mmitova3@gmail.com>
2025-12-18 09:58:12 +0200

    added more solutions

---
+++ number_of_lines_to_write_string.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+        result = []
+        total_width = 0
+        curr_line = 0
+        lines = 0
+
+        for char in s:
+            idx = (ord(char) - 97)
+            total_width += widths[idx]
+            if curr_line + widths[idx] <= 100:
+                curr_line += widths[idx]
+            else:
+                lines += 1
+                curr_line = widths[idx]
+        lines += 1
+
+        result.append(lines)
+        result.append(curr_line)
+
+        return result
+
+
+print(Solution().numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],
+                                s = "bbbcccdddaaa"))
+print(Solution().numberOfLines(widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],
+                               s = "abcdefghijklmnopqrstuvwxyz"))
+
+
+
