cce3007fee31fb9357b94faeeb978a272b866784
mariami57 <mmitova3@gmail.com>
2025-12-19 12:50:44 +0200

    added more solutions

--- positions_of_large_groups.py
+++ positions_of_large_groups.py
@@ -1,24 +1,38 @@
 from typing import List
 
 
+# class Solution:
+#     def largeGroupPositions(self, s: str) -> List[List[int]]:
+#         result = []
+#         i = 1
+#         while i < len(s)-1:
+#             if s[i-1] == s[i] == s[i+1]:
+#                 start_idx = i - 1
+#                 while s[i] == s[start_idx] and i < len(s)-1:
+#                     i+=1
+#                 if i == len(s) - 1 and s[i-1] == s[-1]:
+#                     end_idx = i
+#                 else:
+#                     end_idx = i - 1
+#                 result.append([start_idx, end_idx])
+#             i += 1
+#
+#         return result
+
 class Solution:
     def largeGroupPositions(self, s: str) -> List[List[int]]:
         result = []
-        i = 1
-        while i < len(s)-1:
-            if s[i-1] == s[i] == s[i+1]:
-                start_idx = i - 1
-                while s[i] == s[start_idx] and i < len(s)-1:
-                    i+=1
-                if i == len(s) - 1 and s[i-1] == s[-1]:
-                    end_idx = i
-                else:
-                    end_idx = i - 1
-                result.append([start_idx, end_idx])
-            i += 1
+        start = 0
 
+        while start < len(s):
+            end = start
+            while end < len(s) and s[end] == s[start]:
+                end += 1
+            if end - start >= 3:
+                result.append([start, end-1])
+
+            start = end
         return result
-
 print(Solution().largeGroupPositions(s = "bababbaaab"))
 print(Solution().largeGroupPositions(s = "aaa"))
 print(Solution().largeGroupPositions(s = "abbxxxxzzy"))
