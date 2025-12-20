342440d965bafe9b40a4e61b7a013822c3aa45a0
mariami57 <mmitova3@gmail.com>
2025-12-20 13:40:08 +0200

    added more solutions

--- transpose_matrix.py
+++ transpose_matrix.py
@@ -1,19 +1,29 @@
 from typing import List
 
 
+# class Solution:
+#     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+#         rows = len(matrix)
+#         cols = len(matrix[0])
+#         result = []
+#
+#         for c in range(cols):
+#             temp = []
+#             for r in range(rows):
+#                 temp.append(matrix[r][c])
+#
+#             result.append(temp)
+#         return result
+
+
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        result = []
+        res = [[0]*len(matrix) for _ in range(len(matrix[0]))]
 
-        for c in range(cols):
-            temp = []
-            for r in range(rows):
-                temp.append(matrix[r][c])
-
-            result.append(temp)
-        return result
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                res[c][r] = matrix[r][c]
+        return res
 
 print(Solution().transpose([[1,2,3],[4,5,6],[7,8,9]]))
 print(Solution().transpose([[1,2,3],[4,5,6]]))
