e0ac5ea099711f35984ea5048251e76c169d9b9a
mariami57 <mmitova3@gmail.com>
2026-01-18 19:20:19 +0200

    added more solutions

--- valid_mountain_array.py
+++ valid_mountain_array.py
@@ -1,31 +1,54 @@
 from typing import List
 
 
+# class Solution:
+#     def validMountainArray(self, arr: List[int]) -> bool:
+#         if len(arr) < 3:
+#             return False
+#
+#         end = max(arr)
+#         end_idx = arr.index(end)
+#         if end_idx == len(arr) - 1 or end == arr[0]:
+#             return False
+#
+#         first_half = arr[:end_idx]
+#         second_half = arr[end_idx+1:]
+#
+#         if end in first_half or end in second_half:
+#             return False
+#
+#         for i in range(1, len(first_half)):
+#             if first_half[i-1] >= first_half[i]:
+#                 return False
+#
+#         for i in range(1, len(second_half)):
+#             if second_half[i-1] <= second_half[i]:
+#                 return False
+#
+#         return True
+
+
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr) < 3:
+        start = 1
+        end = len(arr) - 1
+        length = len(arr)
+
+        if length < 3:
             return False
 
-        end = max(arr)
-        end_idx = arr.index(end)
-        if end_idx == len(arr) - 1 or end == arr[0]:
+        while start < length and arr[start] > arr[start-1]:
+            start += 1
+
+        while end > 0 and arr[end] < arr[end-1]:
+            end -= 1
+
+        start -= 1
+
+        if start == length -1 or start == 0:
             return False
 
-        first_half = arr[:end_idx]
-        second_half = arr[end_idx+1:]
-
-        if end in first_half or end in second_half:
-            return False
-
-        for i in range(1, len(first_half)):
-            if first_half[i-1] >= first_half[i]:
-                return False
-
-        for i in range(1, len(second_half)):
-            if second_half[i-1] <= second_half[i]:
-                return False
-
-        return True
+        return start == end
 
 print(Solution().validMountainArray([0,3,2,1]))
 print(Solution().validMountainArray([6,7,7,8,6]))
