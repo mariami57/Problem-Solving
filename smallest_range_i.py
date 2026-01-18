c180034bfbc47698e3880803a992286a810f9267
mariami57 <mmitova3@gmail.com>
2026-01-18 17:30:58 +0200

    added more solutions

---
+++ smallest_range_i.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        max_n = max(nums)
+        min_n = min(nums)
+
+        for _ in range(k):
+            max_n -= 1
+            min_n +=1
+
+            if min_n >= max_n:
+                return 0
+        return max_n - min_n
+
+
+print(Solution().smallestRangeI(nums = [1,3,6], k = 3))
