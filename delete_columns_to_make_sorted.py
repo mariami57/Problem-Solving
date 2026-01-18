9ba4ae8126fec34e5324d378c60625d49e5b42e0
mariami57 <mmitova3@gmail.com>
2026-01-18 20:24:28 +0200

    added more solutions

--- delete_columns_to_make_sorted.py
+++ delete_columns_to_make_sorted.py
@@ -1,26 +1,38 @@
 from typing import List
 
 
+# class Solution:
+#     def minDeletionSize(self, strs: List[str]) -> int:
+#
+#         length = len(strs[0])
+#         rem = 0
+#         total_str = len(strs)
+#         j = 0
+#
+#         while j < length:
+#             i = 1
+#             while i < total_str:
+#                 if ord(strs[i][j]) < ord(strs[i-1][j]):
+#                     rem += 1
+#                     break
+#                 i+=1
+#             i = 0
+#             j += 1
+#
+#         return rem
+
+
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
+        res = 0
 
-        length = len(strs[0])
-        rem = 0
-        total_str = len(strs)
-        j = 0
-
-        while j < length:
-            i = 1
-            while i < total_str:
-                if ord(strs[i][j]) < ord(strs[i-1][j]):
-                    rem += 1
+        for col in range(len(strs[0])):
+            for row in range(1, len(strs)):
+                if strs[row][col] < strs[row-1][col]:
+                    res+=1
                     break
-                i+=1
-            i = 0
-            j += 1
 
-        return rem
-
+        return res
 
 
 print(Solution().minDeletionSize(["zyx","wvu","tsr"]))
